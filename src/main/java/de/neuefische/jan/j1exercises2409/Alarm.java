6b38c83199a95b72ab69001343dc1fc35fa1a469
Jan Robert <janmartinrobert@gmail.com>
2020-09-24 14:39:23 +0200

    Extract maximum number of people to method

--- src/main/java/de/neuefische/jan/j1exercises2409/Alarm.java
+++ src/main/java/de/neuefische/jan/j1exercises2409/Alarm.java
@@ -3,22 +3,23 @@ package de.neuefische.jan.j1exercises2409;
 public class Alarm {
 
     public static String getPeopleCapacityStatus(int numberOfPeople, String threatLevel) {
-        int limit = 0;
-        switch(threatLevel) {
-            case "red":
-                limit = 0;
-                break;
-            case "yellow":
-                limit = 30;
-                break;
-            case "green":
-                limit = 60;
-                break;
-        }
+        int limit = getMaximumNumberOfPeople(threatLevel);
         if (numberOfPeople <= limit) {
             return "Maximum number of people not exceeded";
         }
         return "Too many people";
     }
 
+    public static int getMaximumNumberOfPeople(String threatLevel){
+        switch(threatLevel) {
+            case "red":
+                return 0;
+            case "yellow":
+                return 30;
+            case "green":
+                return 60;
+            default:
+                return 0;
+        }
+    }
 }
