68ad4c9248fca87fd9e8dd8f97f30e5aa3c4343a
Jan Robert <janmartinrobert@gmail.com>
2020-09-24 16:10:48 +0200

    Add exercise faculty with recursion

--- src/main/java/de/neuefische/jan/j1exercises2409/Faculty.java
+++ src/main/java/de/neuefische/jan/j1exercises2409/Faculty.java
@@ -3,12 +3,10 @@ package de.neuefische.jan.j1exercises2409;
 public class Faculty {
 
     public static int faculty(int number){
-        int i = 0;
-        int result = 1;
-        while(i++ < number) {
-            result *= i;
+        if(number == 0){
+            return 1;
         }
-        return result;
+        return number * faculty(number-1);
     }
 
 }
